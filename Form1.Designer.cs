fb0de4edc87e56882052bc1b0f313983bf307183
conrocks1 <connor.rock8@gmail.com>
2016-04-06 20:27:54 +0100

    Added Initial Files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebBrowser
 {
-    partial class Form1
+    partial class SurfNet
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.favouirtesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gitHubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.youTubeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facebookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.twitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.amazonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eBayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonNavigate = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.googleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.favouirtesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(828, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newWindowToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newWindowToolStripMenuItem
+            // 
+            this.newWindowToolStripMenuItem.Name = "newWindowToolStripMenuItem";
+            this.newWindowToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // favouirtesToolStripMenuItem
+            // 
+            this.favouirtesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleToolStripMenuItem,
+            this.gitHubToolStripMenuItem,
+            this.youTubeToolStripMenuItem,
+            this.facebookToolStripMenuItem,
+            this.twitterToolStripMenuItem,
+            this.redditToolStripMenuItem,
+            this.amazonToolStripMenuItem,
+            this.eBayToolStripMenuItem});
+            this.favouirtesToolStripMenuItem.Name = "favouirtesToolStripMenuItem";
+            this.favouirtesToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.favouirtesToolStripMenuItem.Text = "Favouirtes";
+            // 
+            // gitHubToolStripMenuItem
+            // 
+            this.gitHubToolStripMenuItem.Name = "gitHubToolStripMenuItem";
+            this.gitHubToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.gitHubToolStripMenuItem.Text = "GitHub";
+            this.gitHubToolStripMenuItem.Click += new System.EventHandler(this.gitHubToolStripMenuItem_Click);
+            // 
+            // youTubeToolStripMenuItem
+            // 
+            this.youTubeToolStripMenuItem.Name = "youTubeToolStripMenuItem";
+            this.youTubeToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.youTubeToolStripMenuItem.Text = "YouTube";
+            this.youTubeToolStripMenuItem.Click += new System.EventHandler(this.youTubeToolStripMenuItem_Click);
+            // 
+            // facebookToolStripMenuItem
+            // 
+            this.facebookToolStripMenuItem.Name = "facebookToolStripMenuItem";
+            this.facebookToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.facebookToolStripMenuItem.Text = "Facebook";
+            this.facebookToolStripMenuItem.Click += new System.EventHandler(this.facebookToolStripMenuItem_Click);
+            // 
+            // twitterToolStripMenuItem
+            // 
+            this.twitterToolStripMenuItem.Name = "twitterToolStripMenuItem";
+            this.twitterToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.twitterToolStripMenuItem.Text = "Twitter";
+            this.twitterToolStripMenuItem.Click += new System.EventHandler(this.twitterToolStripMenuItem_Click);
+            // 
+            // redditToolStripMenuItem
+            // 
+            this.redditToolStripMenuItem.Name = "redditToolStripMenuItem";
+            this.redditToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.redditToolStripMenuItem.Text = "Reddit";
+            this.redditToolStripMenuItem.Click += new System.EventHandler(this.redditToolStripMenuItem_Click);
+            // 
+            // amazonToolStripMenuItem
+            // 
+            this.amazonToolStripMenuItem.Name = "amazonToolStripMenuItem";
+            this.amazonToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.amazonToolStripMenuItem.Text = "Amazon";
+            this.amazonToolStripMenuItem.Click += new System.EventHandler(this.amazonToolStripMenuItem_Click);
+            // 
+            // eBayToolStripMenuItem
+            // 
+            this.eBayToolStripMenuItem.Name = "eBayToolStripMenuItem";
+            this.eBayToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.eBayToolStripMenuItem.Text = "eBay";
+            this.eBayToolStripMenuItem.Click += new System.EventHandler(this.eBayToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(81, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // buttonNavigate
+            // 
+            this.buttonNavigate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNavigate.Location = new System.Drawing.Point(709, 27);
+            this.buttonNavigate.Name = "buttonNavigate";
+            this.buttonNavigate.Size = new System.Drawing.Size(107, 20);
+            this.buttonNavigate.TabIndex = 2;
+            this.buttonNavigate.Text = " Navigate";
+            this.buttonNavigate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonNavigate.UseVisualStyleBackColor = false;
+            this.buttonNavigate.Click += new System.EventHandler(this.buttonNavigate_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(13, 27);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(28, 20);
+            this.buttonBack.TabIndex = 3;
+            this.buttonBack.Text = "<";
+            this.buttonBack.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Location = new System.Drawing.Point(47, 27);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(28, 20);
+            this.buttonForward.TabIndex = 4;
+            this.buttonForward.Text = ">";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(13, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(803, 420);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser1_ProgressChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 475);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(828, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Location = new System.Drawing.Point(641, 27);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(28, 20);
+            this.buttonStop.TabIndex = 7;
+            this.buttonStop.Text = "X";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(675, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 20);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "R";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // googleToolStripMenuItem
+            // 
+            this.googleToolStripMenuItem.Name = "googleToolStripMenuItem";
+            this.googleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.googleToolStripMenuItem.Text = "Google";
+            this.googleToolStripMenuItem.Click += new System.EventHandler(this.googleToolStripMenuItem_Click);
+            // 
+            // SurfNet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(828, 497);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonNavigate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SurfNet";
+            this.Text = "SurfNet";
+            this.Load += new System.EventHandler(this.SurfNet_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonNavigate;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem favouirtesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gitHubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem youTubeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facebookToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem twitterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem amazonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eBayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWindowToolStripMenuItem;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem googleToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,150 @@ using System.Windows.Forms;
 
 namespace WebBrowser
 {
-    public partial class Form1 : Form
+    public partial class SurfNet : Form
     {
-        public Form1()
+        public SurfNet()
         {
             InitializeComponent();
         }
+
+        // When exit is pressed, this code will execute
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made my TechCypher on the 6/4/16 using C#.Net");
+        }
+
+        private void SurfNet_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+       
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //When enter is pressed, navigate
+            if( e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                NavigateToPage();
+            }
+        }
+
+        
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.CurrentProgress > 0 & e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+            }
+        }
+        #region Faviourites
+        // The following code will execute when the user use the favourite option in the tool strip. 
+        // It will first navigate to the selected page and then change the address bar to the same page.
+        private void gitHubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.github.com");
+            textBox1.Text = "http://www.github.com/";
+        }
+
+        private void youTubeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.youtube.com");
+            textBox1.Text = "http://www.youtube.com/";
+        }
+
+        private void facebookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.facebook.com");
+            textBox1.Text = "http://www.facebook.com/";
+        }
+
+        private void twitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.twitter.com");
+            textBox1.Text = "http://www.twitter.com/";
+        }
+
+        private void redditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.reddit.com");
+            textBox1.Text = "http://www.reddit.com/";
+        }
+
+        private void amazonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.amazon.co.uk");
+            textBox1.Text = "http://www.amazon.co.uk/";
+        }
+
+        private void eBayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.ebay.com");
+            textBox1.Text = "http://www.ebay.com/";
+        }
+
+        private void googleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.google.com");
+            textBox1.Text = "http://www.google.com/";
+        }
+        #endregion
+
+        #region Navigation
+        private void NavigateToPage()
+        {
+            webBrowser1.Navigate(textBox1.Text);
+            buttonNavigate.Enabled = false;
+            textBox1.Enabled = false;
+            toolStripStatusLabel1.Text = "Navigation has started.";
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            buttonNavigate.Enabled = true;
+            textBox1.Enabled = true;
+            toolStripStatusLabel1.Text = "Navigation has complete.";
+            textBox1.Text = webBrowser1.Url.OriginalString;
+        }
+
+        // When navigate button pressed, website loads.
+        private void buttonNavigate_Click(object sender, EventArgs e)
+        {
+            NavigateToPage();
+        }
+
+        //When pressed, the browser will go back
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        //When pressed, the browser will go forward
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        //When pressed, the page will stop loading
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        //When pressed, the page will refresh
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+        #endregion
+
     }
 }
